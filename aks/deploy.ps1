d3318119d79cdc84e07c4cbe90f340a3383dc6a1
Steve Griffith <stgriffi@microsoft.com>
2017-12-05 14:00:15 -0500

    Added aks

---
+++ aks/deploy.ps1
@@ -0,0 +1,9 @@
+﻿Login-AzureRmAccount
+
+$rgName = "<RGName>"
+$location = "<Location>"
+$ArmTemplatePath = ".\azuredeploy.json"
+$additionalParams = ".\azuredeploy.parameters.json"
+
+New-AzureRmResourceGroup -Name $rgName -Location $location
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $ArmTemplatePath -TemplateParameterFile $additionalParams -DeploymentDebugLogLevel All
