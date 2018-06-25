53e07eea63bb6743d43a81c92d9382f4018a52a8
Steve Griffith <steve.griffith@microsoft.com>
2018-06-25 15:40:27 +0000

    Added files for App GW Test

---
+++ appgw-ssl/CertCreation.ps1
@@ -0,0 +1,8 @@
+﻿New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname www.contoso2.com
+
+$pwd = ConvertTo-SecureString -String "<insert your passwd>" -Force -AsPlainText
+Export-PfxCertificate -cert cert:\localMachine\my\<insert thumbprint from creation> -FilePath c:\appgwcert2.pfx -Password $pwd
+
+#Use the following to get the Cert Data for the parameters file. 
+#Note that it may have carriage returns that you'll need to remove before putting in the params file
+[System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("c:\appgwcert2.pfx"))
